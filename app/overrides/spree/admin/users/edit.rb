5537351e01579de9022bbe3585d3c87644642b19
Jeff Klink <jklink@nationalprostaff.com>
2017-11-07 15:32:16 -0500

    Removing refilliate fields for referral

--- app/overrides/spree/admin/users/edit.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: "spree/admin/users/edit",
-  name: "referral_fieldset",
-  insert_after: "[data-hook='admin_user_api_key']",
-  partial: "spree/admin/users/referral_fieldset"
-)
